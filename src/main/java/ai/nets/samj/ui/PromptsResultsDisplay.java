fabfd24b94e8673bb2990b245fd7945a6e4946e9
carlosuc3m <100329787@alumnos.uc3m.es>
2024-03-14 21:15:04 +0100

    remove unnecessary method

--- src/main/java/ai/nets/samj/ui/PromptsResultsDisplay.java
+++ src/main/java/ai/nets/samj/ui/PromptsResultsDisplay.java
@@ -131,11 +131,6 @@ public interface PromptsResultsDisplay {
 	Object getFocusedImage();
 	
 	/**
-	 * Uses the created ROIs to create an instance segmentation mask
-	 */
-	void createInstanceSegmentationMask();
-	
-	/**
 	 * This method uses the exception launched by SAMJ to display an understandable error message in the consumer software
 	 * @param type
 	 * 	which kind of exception is being throw from the possibilities of {@link SAMJException}

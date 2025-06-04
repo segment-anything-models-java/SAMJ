e8c9c5c8a296de68235b937cb925420bb49698d1
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-04 14:26:30 +0200

    correct error

--- src/main/java/ai/nets/samj/models/Sam2.java
+++ src/main/java/ai/nets/samj/models/Sam2.java
@@ -76,7 +76,7 @@ public class Sam2 extends AbstractSamJ {
 			+ "measure.label(np.ones((10, 10)), connectivity=1)" + System.lineSeparator()
 			+ "import torch" + System.lineSeparator()
 			+ "device = 'cpu'" + System.lineSeparator()
-			+ ((!IS_APPLE_SILICON && false) ? "" // TODO Add a button so the user can decide whether to use accelerators or not (I tried enabling by default and some models might be out of memory)
+			+ ((!IS_APPLE_SILICON || true) ? "" // TODO Add a button so the user can decide whether to use accelerators or not (I tried enabling by default and some models might be out of memory)
 					: "from torch.backends import mps" + System.lineSeparator()
 					+ "if mps.is_built() and mps.is_available():" + System.lineSeparator()
 					+ "  device = 'mps'" + System.lineSeparator())
